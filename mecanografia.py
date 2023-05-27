055bdd41d7142ad41335d15560e5575197973267
DirceuPC <dirceulzn@gmail.com>
2023-05-27 14:16:18 +0200

    Organizar formato texto print.

--- mecanografia.py
+++ mecanografia.py
@@ -13,13 +13,16 @@ def mecanografia(texto):  # funcion para el efecto de mecanografia
 
 textos = llamar_repuestas()
 
+
+color_verde = '\033[92m'
+color_reset = '\033[0m'
+longitud = 40  # es el total de caracteres por línea de impresion
+relleno = " "  # rellena el texto con espacios en blanco
+
 for x in textos:
-    color_verde = '\033[92m'
-    color_reset = '\033[0m'
-    longitud = 40  # es el total de caracteres por líena de impresion
-    relleno = " "  # rellena el texto con espacios en blanco
     preguntas = x[0]
     respuestas = x[1]
+
 
     if preguntas != 'preguntas':
         print(color_verde + preguntas.rjust(longitud,
